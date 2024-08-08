735edd63c234623daf9a3325d878ead32a4bcf3e
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 10:01:37 +0530

    Update 15.c

--- ASSIGNMENT 2/15.c
+++ ASSIGNMENT 2/15.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    
+    if (argc != 2) {
+        printf("Usage: %s <string>\n", argv[0]);
+        return 1; 
+    }
+
+    char *str = argv[1];  
+    int length = strlen(str);  
+
+ 
+    printf("Original string: %s\n", str);
+
+   
+    printf("Reversed string: ");
+    for (int i = length - 1; i >= 0; i--) {
+        putchar(str[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
 
