ac61ab1f7902f0fd7848691867557d5d39aae3bb
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 10:03:21 +0530

    Update 18.c

--- ASSIGNMENT 2/18.c
+++ ASSIGNMENT 2/18.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    
+    const int EXPECTED_ARGS = 3; 
+
+    
+    if (argc != EXPECTED_ARGS) {
+        printf("Error: Incorrect number of arguments.\n");
+        printf("Usage: %s <arg1> <arg2>\n", argv[0]);
+        return 1; 
+    }
+
+    
+    printf("Correct number of arguments provided.\n");
+    printf("Argument 1: %s\n", argv[1]);
+    printf("Argument 2: %s\n", argv[2]);
+
+    return 0;
+}
 
