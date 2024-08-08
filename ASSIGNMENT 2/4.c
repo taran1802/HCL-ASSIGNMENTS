7e1cbfa90ac0e0a6215dccc655bba304d85801dd
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 09:48:47 +0530

    Update 4.c

--- ASSIGNMENT 2/4.c
+++ ASSIGNMENT 2/4.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};  
+    int *ptr = arr;               
+    int size = sizeof(arr) / sizeof(arr[0]);  
+    int sum = 0;                  
+
+   
+    for (int i = 0; i < size; i++) {
+        sum += *(ptr + i);  
+    }
+
+    
+    printf("Sum of array elements: %d\n", sum);
+
+    return 0;
+}
 
