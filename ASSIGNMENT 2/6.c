e643d8cb39b34a6841b34450f4a64d0c3dd0cc7e
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 09:50:22 +0530

    Update 6.c

--- ASSIGNMENT 2/6.c
+++ ASSIGNMENT 2/6.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+
+
+void findMinMax(int *arr, int size, int *min, int *max) {
+    *min = *max = *arr;  
+
+    for (int i = 1; i < size; i++) {
+        if (*(arr + i) < *min) {
+            *min = *(arr + i);  
+        }
+        if (*(arr + i) > *max) {
+            *max = *(arr + i);  
+        }
+    }
+}
+
+int main() {
+    int arr[] = {12, 45, 7, 32, 89, 54, 23};  
+    int size = sizeof(arr) / sizeof(arr[0]);  
+    int min, max;                           
+
+    
+    findMinMax(arr, size, &min, &max);
+
+    printf("Array elements:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    printf("Smallest element: %d\n", min);
+    printf("Largest element: %d\n", max);
+
+    return 0;
+}
 
