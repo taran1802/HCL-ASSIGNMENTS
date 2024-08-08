a7a7d30688f9c4d59882874700e7b03dde4d3b23
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 09:48:14 +0530

    Update 3.c

--- ASSIGNMENT 2/3.c
+++ ASSIGNMENT 2/3.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};  
+    int *ptr = arr;               
+    int size = sizeof(arr) / sizeof(arr[0]);  
+
+    
+    printf("Array elements:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", *(ptr + i));  
+    }
+    printf("\n");
+
+    return 0;
+}
 
