9c7f0f557b90086451511af6d87e2c3c3743b065
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 10:04:58 +0530

    Update 21.c

--- ASSIGNMENT 2/21.c
+++ ASSIGNMENT 2/21.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    
+    if (argc != 2) {
+        printf("Usage: %s <ENV_VARIABLE_NAME>\n", argv[0]);
+        return 1; 
+    }
+
+    
+    const char *env_var_name = argv[1];
+
+    
+    const char *env_var_value = getenv(env_var_name);
+
+    
+    if (env_var_value != NULL) {
+        printf("%s=%s\n", env_var_name, env_var_value);
+    } else {
+        printf("Environment variable '%s' not found.\n", env_var_name);
+    }
+
+    return 0;
+}
 
