604b840e6570b5940ce6c5db14992bc0781747e7
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 09:58:48 +0530

    Update 12.c

--- ASSIGNMENT 2/12.c
+++ ASSIGNMENT 2/12.c
@@ -1,1 +1,12 @@
 
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    
+    printf("Command-line arguments:\n");
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+
+    return 0;
+}
