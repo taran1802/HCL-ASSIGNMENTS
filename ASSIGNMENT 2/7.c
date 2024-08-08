4912afa1f98e1fb8b6479fe2c0e6d761b980504b
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 09:51:02 +0530

    Update 7.c

--- ASSIGNMENT 2/7.c
+++ ASSIGNMENT 2/7.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+// Function that adds two integers
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+   
+    int (*func_ptr)(int, int) = add;
+    
+    int num1 = 5;
+    int num2 = 7;
+    int result;
+
+    
+    result = func_ptr(num1, num2);
+
+
+    printf("The sum of %d and %d is %d\n", num1, num2, result);
+
+    return 0;
+}
 
