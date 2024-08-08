f3ad0d9b77a3517a6e885b9b12c812c41840c2a0
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 10:15:09 +0530

    Update 5.sh

--- ASSIGNMENT1/5.sh
+++ ASSIGNMENT1/5.sh
@@ -1,1 +1,45 @@
+#!/bin/bash
+
+
+trap '' 2 20
+
+
+echo -n "Enter password: "
+stty -echo
+read password1
+echo
+
+
+echo -n "Confirm password: "
+read password2
+echo
+stty echo
+
+
+if [ "$password1" != "$password2" ]; then
+    echo "Passwords do not match. Exiting..."
+    exit 1
+fi
+
+echo "Terminal locked. Enter the password to unlock."
+
+
+while true; do
+    stty -echo
+    echo -n "Enter password to unlock: "
+    read attempt
+    echo
+    stty echo
+
+    if [ "$attempt" == "$password1" ]; then
+        echo "Terminal unlocked."
+        break
+    else
+        echo "Incorrect password. Try again."
+    fi
+done
+
+
+stty echo
+trap 2 20
 
