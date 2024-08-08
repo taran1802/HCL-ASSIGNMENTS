8d714ef2f62a915a4b2d40ee867f35a448933e6b
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 10:03:55 +0530

    Update 19.c

--- ASSIGNMENT 2/19.c
+++ ASSIGNMENT 2/19.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+int is_valid_integer(const char *str) {
+    
+    if (str == NULL || *str == '\0') {
+        return 0;
+    }
+
+  
+    for (size_t i = 0; i < strlen(str); i++) {
+        if (!isdigit(str[i]) && (i != 0 || str[i] != '-' || strlen(str) == 1)) {
+            return 0; 
+        }
+    }
+
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    
+    if (argc < 2) {
+        printf("Usage: %s <int1> <int2> ... <intN>\n", argv[0]);
+        return 1;
+    }
+
+    
+    for (int i = 1; i < argc; i++) {
+        if (is_valid_integer(argv[i])) {
+            printf("Argument %d is a valid integer: %s\n", i, argv[i]);
+        } else {
+            printf("Error: Argument %d is not a valid integer: %s\n", i, argv[i]);
+            return 1; 
+        }
+    }
+
+    return 0;
+}
 
