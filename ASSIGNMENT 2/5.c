317be0d739e759f372081d431513576794761660
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 09:49:50 +0530

    Update 5.c

--- ASSIGNMENT 2/5.c
+++ ASSIGNMENT 2/5.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+
+
+void reverseArray(int *arr, int size) {
+    int *start = arr;           
+    int *end = arr + size - 1; 
+    int temp;
+
+    while (start < end) {
+       
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+       
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};  
+    int size = sizeof(arr) / sizeof(arr[0]);  
+
+    
+    printf("Array before reversing:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+   
+    reverseArray(arr, size);
+
+    
+    printf("Array after reversing:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
 
