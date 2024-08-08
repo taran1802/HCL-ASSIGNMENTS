bf12b5d9a5066b65b9db5a62c34cb8493d7532ea
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 10:04:24 +0530

    Update 20.c

--- ASSIGNMENT 2/20.c
+++ ASSIGNMENT 2/20.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+extern char **environ; 
+
+int main() {
+    char **env = environ; 
+
+    
+    
+    while (*env != NULL) {
+        printf("%s\n", *env);
+        env++;
+    }
+
+    return 0;
+}
 
