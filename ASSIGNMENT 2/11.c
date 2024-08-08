18645154c84569bd6aaa3ceb614246f5602219d7
taran1802 <145441816+taran1802@users.noreply.github.com>
2024-08-08 09:57:31 +0530

    Update 11.c

--- ASSIGNMENT 2/11.c
+++ ASSIGNMENT 2/11.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    
+    
+    printf("Number of command-line arguments: %d\n", argc);
+
+    
+    printf("Command-line arguments:\n");
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
